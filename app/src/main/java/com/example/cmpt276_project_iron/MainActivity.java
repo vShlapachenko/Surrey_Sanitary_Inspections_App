8d764a8f2cd3a6ac8ace2a6acd37fc6ba6b5fdcc
vShlapachenko <valentyn.shlapachenko@gmail.com>
2020-03-02 15:07:26 -0800

    Created Restaurant & Restaurant manager class, added csv files for future purpose

--- app/src/main/java/com/example/cmpt276_project_iron/MainActivity.java
+++ app/src/main/java/com/example/cmpt276_project_iron/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.cmpt276_project_iron;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.cmpt276_project_iron.model.Restaurant;
+import com.example.cmpt276_project_iron.model.RestaurantManager;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RestaurantManager manager = RestaurantManager.getInstance();
     }
 }
