288cb34aef5890c17fb7feba42a9dbc1a635a00d
vShlapachenko <valentyn.shlapachenko@gmail.com>
2020-03-02 18:48:00 -0800

    Added Conversion classes
    (with temporary function implementation so other can work on the project)

--- app/src/main/java/com/example/cmpt276_project_iron/model/RestaurantManager.java
+++ app/src/main/java/com/example/cmpt276_project_iron/model/RestaurantManager.java
@@ -2,16 +2,20 @@ package com.example.cmpt276_project_iron.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class RestaurantManager{
 
     private static RestaurantManager instance;
-    private static List<Restaurant> restaurantList;
+    private static List<Restaurant> restaurantList = new ArrayList<>();
 
     public static RestaurantManager getInstance() {
         if (instance == null){
             instance = new RestaurantManager();
+            CSVConverterForRestaurant converter = new CSVConverterForRestaurant();
+            converter.convertRestaurantCSVToList();
+            Collections.sort(restaurantList);
         }
         return instance;
     }
@@ -23,29 +27,14 @@ public class RestaurantManager{
                 '}';
     }
 
-    public static List<Restaurant> getRestaurantList() {
+    private List<Restaurant> getRestaurantList(){
         return restaurantList;
     }
 
-    /*
+    public void add(String trackingNumber, String name, String physicalAddress, String physicalCity,
+                    String facType, double latitude, double longitude){
+        restaurantList.add(new Restaurant(trackingNumber, name, physicalAddress, physicalCity,
+                facType, latitude, longitude));
+    }
 
-   Arrays.asList(
-                new Restaurant("SDFO-8HKP7E","Pattullo A&W","12808 King George Blvd",
-                        "Surrey","Restaurant",49.20610961,-122.8668064),
-                new Restaurant("SHEN-B7BNSR","Lee Yuen Seafood Restaurant","1812 152 St",
-                        "Surrey","Restaurant",49.03508252,-122.80086843),
-                new Restaurant("NOSU-CHNUM","The Unfindable Bar","12345 67 Ave",
-                        "Surrey","Restaurant",49.14214908,-122.86815856),
-                new Restaurant("SWOD-AHZUMF","Lee Yuen Seafood Restaurant","14755 104 Ave",
-                        "Surrey","Restaurant",49.19166808,-122.8136896),
-                new Restaurant("SDFO-8GPUJX","Top in Town Pizza","12788 76A Ave",
-                        "Surrey","Restaurant",49.14218908,-122.86855856),
-                new Restaurant("SWOD-APSP3X","104 Sushi & Co.","10422 168 St",
-                        "Surrey","Restaurant",49.19205936,-122.75625586),
-                new Restaurant("SHEN-ANSLB4","Top In Town Pizza","14330 64 Ave",
-                        "Surrey","Restaurant",49.11851305,-122.82521495),
-                new Restaurant("SPLH-9NEUHG","Zugba Flame Grilled Chicken","14351 104 Ave",
-                        "Surrey","Restaurant",49.19172759,-122.82418348)
-        );
-     */
 }
