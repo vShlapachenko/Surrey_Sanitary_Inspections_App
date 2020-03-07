eb33b8dfd6c62acdef6b48b2222cf75454a4e168
vShlapachenko <valentyn.shlapachenko@gmail.com>
2020-03-07 14:57:02 -0800

    Added date conversion class
    Works correct ready to be implemented

---
+++ app/src/main/java/com/example/cmpt276_project_iron/model/DateConversionCalculator.java
@@ -0,0 +1,33 @@
+package com.example.cmpt276_project_iron.model;
+
+import android.content.Context;
+
+import com.example.cmpt276_project_iron.R;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+
+public class DateConversionCalculator {
+
+    private static final int MONTHS_IN_A_YEAR = 12;
+    private static final int DAYS_THRESHOLD = 30;
+
+    public static String getFormattedDate(Context context, Calendar givenDate){
+        Calendar today = Calendar.getInstance();
+        int inspectionYear = givenDate.get(Calendar.YEAR);
+        int inspectionMonth = givenDate.get(Calendar.MONTH);
+        long daysBetween = ChronoUnit.DAYS.between(givenDate.toInstant(), today.toInstant());
+        int yearsDiff = today.get(Calendar.YEAR) - inspectionYear;
+        int monthDiff = today.get(Calendar.MONTH) - inspectionMonth;
+        int monthsBetween = yearsDiff * MONTHS_IN_A_YEAR + monthDiff;
+        if (daysBetween <= DAYS_THRESHOLD){
+            return Long.toString(daysBetween);
+        } else if(monthsBetween <= MONTHS_IN_A_YEAR) {
+            String[] monthArray = context.getResources().getStringArray(R.array.months);
+            return monthArray[inspectionMonth] + inspectionYear;
+        } else {
+            String[] monthArray = context.getResources().getStringArray(R.array.months);
+            return monthArray[inspectionMonth] + inspectionYear;
+        }
+    }
+}
