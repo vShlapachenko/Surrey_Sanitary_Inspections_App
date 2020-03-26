ddbe900e6252c955f9d81400625659ad140fffa4
vShlapachenko <valentyn.shlapachenko@gmail.com>
2020-03-25 18:28:26 -0700

    Finished work on downloading/initial boot activity

--- app/src/main/java/com/example/cmpt276_project_iron/ui/InitialBoot.java
+++ app/src/main/java/com/example/cmpt276_project_iron/ui/InitialBoot.java
@@ -1,10 +1,12 @@
 package com.example.cmpt276_project_iron.ui;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -19,8 +21,10 @@ import com.google.gson.JsonPrimitive;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.DateFormat;
@@ -29,7 +33,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.stream.Collectors;
 
-public class InitialBoot extends AppCompatActivity {
+public class InitialBoot extends AppCompatActivity implements CallBackInquiryFragment {
     private static final int UPDATE_THRESHOLD = 20;
     private static final String RESTAURANTS_FILE_TIME_STAMP = "restaurants_time_stamp.txt";
     private static final String INSPECTIONS_FILE_TIME_STAMP = "inspections_time_stamp.txt";
@@ -37,14 +41,18 @@ public class InitialBoot extends AppCompatActivity {
     private static final String JSON_INSPECTIONS_LAST_MODIFIED = "inspections_last_modified.json";
     private static final String RESTAURANTS_FILE = "restaurants.csv";
     private static final String INSPECTIONS_FILE = "inspections.csv";
+    private static final String RESTAURANTS_FILE_BACKUP = "restaurants_backup.csv";
+    private static final String INSPECTIONS_FILE_BACKUP = "inspections_backup.csv";
     private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
     private static final int END_INDEX_TIME_FORMAT = 19;
     private static final int START_INDEX_TIME_FORMAT = 0;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_boot);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         //Uncomment these lines to wipe internal data
 
@@ -53,7 +61,10 @@ public class InitialBoot extends AppCompatActivity {
         getFileStreamPath(RESTAURANTS_FILE).delete();
         getFileStreamPath(INSPECTIONS_FILE).delete();
         getFileStreamPath(JSON_RESTAURANTS_LAST_MODIFIED).delete();
-        getFileStreamPath(JSON_INSPECTIONS_LAST_MODIFIED).delete();
+        getFileStreamPath(JSON_RESTAURANTS_LAST_MODIFIED).delete();
+        getFileStreamPath(RESTAURANTS_FILE_BACKUP).delete();
+        getFileStreamPath(INSPECTIONS_FILE_BACKUP).delete();
+
         downloadCsvFiles();
     }
 
@@ -105,9 +116,9 @@ public class InitialBoot extends AppCompatActivity {
         }
     }
 
-    private void showFragmentInquiry(String jsonRestaurantUrl, String jsonInspectionUrl) {
+    private void showFragmentInquiry(String csvRestaurantUrl, String csvInspectionUrl) {
         FragmentManager manager = getSupportFragmentManager();
-        InquiryFragment dialog = new InquiryFragment(this, jsonRestaurantUrl, jsonInspectionUrl);
+        InquiryFragment dialog = new InquiryFragment(this, csvRestaurantUrl, csvInspectionUrl, this);
         dialog.show(manager, "MessageDialog");
     }
 
@@ -131,11 +142,11 @@ public class InitialBoot extends AppCompatActivity {
         startActivity(intent);
     }
 
-
-    public void startRestaurantList(View view){
-        Intent intent = new Intent(this, RestaurantList.class);
-        startActivity(intent);
+    @Override
+    public void callBackMethod(String restaurantUrl, String inspectionUrl) {
+        new CsvTask().execute(restaurantUrl, inspectionUrl);
     }
+
 
     private class DateAndUrl {
         String dateRestaurant;
@@ -231,6 +242,105 @@ public class InitialBoot extends AppCompatActivity {
                 throw new IllegalArgumentException("failed to write to " +
                         "internal storage", e);
             }
+        }
+    }
+
+    private class CsvTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(InitialBoot.this);
+            progressDialog.setMessage("Downloading and executing CSV files");
+            progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, InitialBoot.this.getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    CsvTask.this.cancel(true);
+                }
+            });
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        protected String doInBackground(String... params) {
+            String content = getStringFromUrl(params[0]);
+            writeToInternal(content, RESTAURANTS_FILE);
+            content = getStringFromUrl(params[1]);
+            writeToInternal(content, INSPECTIONS_FILE);
+            return content;
+        }
+
+        private String getStringFromUrl(String csvRestaurantUrl) {
+            try (BufferedReader reader = getBufferedReader(csvRestaurantUrl)) {
+                return reader.lines()
+                        .collect(Collectors.joining("\n"));
+            } catch (IOException e) {
+                throw new IllegalArgumentException("failed to read from URL", e);
+            }
+        }
+
+        private BufferedReader getBufferedReader(String csvRestaurantUrl) throws IOException {
+            URL restaurantURL = new URL(csvRestaurantUrl);
+            InputStreamReader isr = new InputStreamReader(restaurantURL.openStream());
+            return new BufferedReader(isr);
+        }
+
+        private void writeToInternal(String content, String file) {
+            try (FileOutputStream fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)) {
+                fileOutputStream.write(content.getBytes());
+            } catch (IOException e) {
+                throw new IllegalArgumentException("failed to write to " +
+                        "internal storage", e);
+            }
+        }
+
+        private void copy(String fileFrom, String fileTo){
+            try (FileOutputStream fileOutputStream = openFileOutput(fileTo, Context.MODE_PRIVATE);
+                 FileInputStream fileInputStream = openFileInput(fileFrom)) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = fileInputStream.read(buffer)) > 0){
+                    fileOutputStream.write(buffer, 0, length);
+                }
+            } catch (IOException e) {
+                Log.e("AsyncTask CsvTask", "Error copy() internal to internal", e);
+            }
+        }
+
+        private void copyFromRawToInternal(InputStream rawRecourseStream, String fileTo){
+            try(FileOutputStream fileOutputStream = openFileOutput(fileTo, Context.MODE_PRIVATE)){
+                byte[] buffer = new byte[1024];
+                int length;
+                while((length = rawRecourseStream.read(buffer)) > 0){
+                    fileOutputStream.write(buffer, 0, length);
+                }
+            } catch (IOException e) {
+                Log.e("AsyncTask CsvTask", "Error in copyFromRawToInternal()", e);
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            if (fileExist(RESTAURANTS_FILE_BACKUP) && fileExist(INSPECTIONS_FILE_BACKUP)) {
+                copy(RESTAURANTS_FILE_BACKUP, RESTAURANTS_FILE);
+                copy(INSPECTIONS_FILE_BACKUP, INSPECTIONS_FILE);
+            } else {
+                copyFromRawToInternal(getResources().openRawResource(R.raw.restaurants_itr1), RESTAURANTS_FILE);
+                copyFromRawToInternal(getResources().openRawResource(R.raw.inspectionreports_itr1), INSPECTIONS_FILE);
+            }
+            startRestaurantListVOID();
+            super.onCancelled();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            copy(RESTAURANTS_FILE, RESTAURANTS_FILE_BACKUP);
+            copy(INSPECTIONS_FILE, INSPECTIONS_FILE_BACKUP);
+            if (progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
+            startRestaurantListVOID();
         }
     }
 }
--- app/src/main/java/com/example/cmpt276_project_iron/ui/InquiryFragment.java
+++ app/src/main/java/com/example/cmpt276_project_iron/ui/InquiryFragment.java
@@ -2,11 +2,9 @@ package com.example.cmpt276_project_iron.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,35 +14,26 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.example.cmpt276_project_iron.R;
-import com.example.cmpt276_project_iron.model.Restaurant;
-
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.stream.Collectors;
 
 public class InquiryFragment extends AppCompatDialogFragment {
 
-    private static final String RESTAURANTS_FILE = "restaurants.csv";
-    private static final String INSPECTIONS_FILE = "inspections.csv";
-
-    private ProgressDialog progressDialog;
     private final Context context;
-    private final String jsonRestaurantUrl;
-    private final String jsonInspectionUrl;
+    private final String csvRestaurantUrl;
+    private final String csvInspectionUrl;
+    private final CallBackInquiryFragment callBackInquiryFragment;
 
-    public InquiryFragment(Context context, String jsonRestaurantUrl, String jsonInspectionUrl) {
+    public InquiryFragment(Context context, String csvRestaurantUrl, String csvInspectionUrl, CallBackInquiryFragment callBackInquiryFragment) {
         this.context = context;
-        this.jsonRestaurantUrl = jsonRestaurantUrl;
-        this.jsonInspectionUrl = jsonInspectionUrl;
+        this.csvRestaurantUrl = csvRestaurantUrl;
+        this.csvInspectionUrl = csvInspectionUrl;
+        this.callBackInquiryFragment = callBackInquiryFragment;
     }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         setCancelable(false);
+
         //Create View to show
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.inquire_data_update_restaurant, null);
 
@@ -55,16 +44,19 @@ public class InquiryFragment extends AppCompatDialogFragment {
         return new AlertDialog.Builder(getActivity())
                 .setTitle("Permission to download updated CSV files")
                 .setView(view)
-                .setPositiveButton(android.R.string.ok, listener)
-                .setNegativeButton(android.R.string.cancel, listener)
+                .setPositiveButton(getString(R.string.yes_positive), listener)
+                .setNegativeButton(getString(R.string.negative_update), listener)
                 .create();
+
     }
 
     private DialogInterface.OnClickListener getOnClickListener() {
         return (dialog, which) -> {
             switch (which) {
                 case DialogInterface.BUTTON_POSITIVE:
-                    new CsvTask(context).execute(jsonRestaurantUrl, jsonInspectionUrl);
+                    if (callBackInquiryFragment != null) {
+                        callBackInquiryFragment.callBackMethod(csvRestaurantUrl, csvInspectionUrl);
+                    }
                     break;
                 case DialogInterface.BUTTON_NEGATIVE:
                     Intent intent = new Intent(context, RestaurantList.class);
@@ -72,63 +64,5 @@ public class InquiryFragment extends AppCompatDialogFragment {
                     break;
             }
         };
-    }
-
-    private class CsvTask extends AsyncTask<String, String, String> {
-
-        Context context;
-
-        public CsvTask (Context context){
-            this.context = context;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = new ProgressDialog(context);
-            progressDialog.setMessage("Downloading and executing CSV files");
-            progressDialog.setCancelable(false);
-            progressDialog.show();
-        }
-
-        protected String doInBackground(String... params) {
-            String content = getStringFromUrl(params[0]);
-            writeToInternal(content, RESTAURANTS_FILE);
-            content = getStringFromUrl(params[1]);
-            writeToInternal(content, INSPECTIONS_FILE);
-            return content;
-        }
-
-        private String getStringFromUrl(String csvRestaurantUrl) {
-            try (BufferedReader reader = getBufferedReader(csvRestaurantUrl)) {
-                return reader.lines()
-                        .collect(Collectors.joining("\n"));
-            } catch (IOException e) {
-                throw new IllegalArgumentException("failed to read from URL", e);
-            }
-        }
-
-        private BufferedReader getBufferedReader(String csvRestaurantUrl) throws IOException {
-            URL restaurantURL = new URL(csvRestaurantUrl);
-            InputStreamReader isr = new InputStreamReader(restaurantURL.openStream());
-            return new BufferedReader(isr);
-        }
-
-        private void writeToInternal(String content, String file) {
-            try (FileOutputStream fileOutputStream = context.openFileOutput(file, Context.MODE_PRIVATE)) {
-                fileOutputStream.write(content.getBytes());
-            } catch (IOException e) {
-                throw new IllegalArgumentException("failed to write to " +
-                        "internal storage", e);
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if (progressDialog.isShowing()) {
-                progressDialog.dismiss();
-            }
-        }
     }
 }
